0228a34d85396b1ab036213a1d3ce20c7993643f
murshid0987 <58227620+murshid0987@users.noreply.github.com>
2022-08-01 15:52:51 +0530

    Add files via upload

---
+++ omni_7305F.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from 7305F device
+$(call inherit-product, device/lenovo/7305F/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := 7305F
+PRODUCT_NAME := omni_7305F
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo TB-7305F
+PRODUCT_MANUFACTURER := lenovo
